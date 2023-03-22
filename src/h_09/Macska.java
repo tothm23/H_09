9c7ebd429f9227aa8ca32d6ea8081b0125c31a3f
tothm23 <tothm.113sz@acsjszki.hu>
2023-03-22 20:14:54 +0100

    Finish

---
+++ src/h_09/Macska.java
@@ -0,0 +1,64 @@
+package h_09;
+
+/**
+ *
+ * @author asus
+ */
+public class Macska {
+
+    private String name;
+    private Integer age;
+    private Boolean male;
+
+    // private final Integer life = 9;
+    private Integer life = 9;
+    private Boolean alombaKakil;
+
+    public Macska(String name, Integer age, Boolean male, Boolean alombaKakil) {
+        this.name = name;
+        this.age = age;
+        this.male = male;
+        this.alombaKakil = alombaKakil;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Boolean getMale() {
+        return male;
+    }
+
+    public void setMale(Boolean male) {
+        this.male = male;
+    }
+
+    public Integer getLife() {
+        return life;
+    }
+
+    public void setLife(Integer life) {
+        this.life = life;
+    }
+
+    public Boolean getAlombaKakil() {
+        return alombaKakil;
+    }
+
+    public void setAlombaKakil(Boolean alombaKakil) {
+        this.alombaKakil = alombaKakil;
+    }
+
+}
